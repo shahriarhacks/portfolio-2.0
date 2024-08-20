7e4552f2e83642888e3a8eddfc439b2dbbdcafa9
SHAHRIAR AHMED <shahirarspyy@gmail.com>
2024-08-20 16:19:28 +0600

    Tap section add

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,46 @@
+import StartIcon from "@/assets/icons/star.svg";
+const words = [
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Friendly",
+  "Responsive",
+  "Maintainable",
+  "Robust",
+  "Search Optimized",
+  "Fast",
+  "Usable",
+  "Reliable",
+  "Efficient",
+  "Dynamic",
+  "Concurrent",
+  "Modular",
+  "High Availability",
+  "Extensible",
+  "Data-Driven",
+  "Asynchronous",
+  "Innovative",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="py-16 lg:py-24 overflow-x-clip">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400  -rotate-3 -mx-1">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-4 py-3">
+            {words.map((word, idx) => (
+              <div key={idx} className="inline-flex items-center gap-4">
+                <span className="text-gray-900 uppercase font-extrabold text-sm">
+                  {word}
+                </span>
+                <StartIcon className="size-6 text-gray-900 -rotate-12" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
