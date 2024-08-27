68f79dff8f8215a4b4fb0e8e4c0b1106e1ff0395
SHAHRIAR AHMED <shahirarspyy@gmail.com>
2024-08-27 19:40:49 +0600

    fix padding

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -54,7 +54,7 @@ export const TestimonialsSection = () => {
         />
 
         <div className="mt-12 lg:mt-20 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] py-4 -my-4">
-          <div className="flex gap-8 flex-none animate-move-left [animation-duration:88s] hover:[animation-play-state:paused]">
+          <div className="flex gap-8 pr-8 flex-none animate-move-left [animation-duration:88s] hover:[animation-play-state:paused]">
             {[...new Array(2)].map((_, idx) => (
               <Fragment key={idx}>
                 {testimonials.map((testimonial) => (
